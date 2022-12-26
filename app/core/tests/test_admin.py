b1db1f5e4eb15e3c2ab8a660e070aa5b9b531275
Regis <regis.martini@travelperk.com>
2022-12-26 18:07:43 +0100

    adding documentation

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,14 +34,14 @@ class AdminSiteTests(TestCase):
 
     def test_edit_user_page(self):
         """Test the edit user page wroks."""
-        url =  reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test create user page."""
-        url =  reverse('admin:core_user_add')
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
